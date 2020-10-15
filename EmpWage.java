90914716d57767774042ddb7fd43cb8b1b1b30e6
Shubham Rana <ranashubham7296@gmail.com>
2020-10-15 17:50:43 +0530

    Added part time employee and daily wage for them

--- EmpWage.java
+++ EmpWage.java
@@ -4,16 +4,18 @@ public class EmpWage {
 	
 	static final int WAGE_PER_HOUR = 20;
 	static final int FULL_DAY_HRS=8;
-	
+	static final int PART_TIME_HRS=4;
 	public static void main(String[] args) {
 		
 		System.out.println("Welcome to Emp Wage computation");
-		int empAttendance = (int)Math.floor(Math.random()*10)%2;
+		int empAttendance = (int)Math.floor(Math.random()*10)%3;
 		
 		if (empAttendance == 1)
-		{
-			System.out.println("Daily wage for today is: "+WAGE_PER_HOUR*FULL_DAY_HRS);
-		}
+			System.out.println("Daily wage for today as full time emp is: "+WAGE_PER_HOUR*FULL_DAY_HRS);
+		else if (empAttendance == 2)
+			System.out.println("Daily wage for today as part time emp is: "+WAGE_PER_HOUR*PART_TIME_HRS);
+		else
+			System.out.println("Daily wage for today is 0");
 	}
 
 }
