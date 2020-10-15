21663bc9b19aff7f4ac0d7a6ef2c000f81aeaef7
Shubham Rana <ranashubham7296@gmail.com>
2020-10-15 17:36:48 +0530

    Calculating daily wage is employee is present

--- EmpWage.java
+++ EmpWage.java
@@ -2,11 +2,18 @@ package EmpWage_JAVA;
 
 public class EmpWage {
 	
+	static final int WAGE_PER_HOUR = 20;
+	static final int FULL_DAY_HRS=8;
+	
 	public static void main(String[] args) {
 		
 		System.out.println("Welcome to Emp Wage computation");
 		int empAttendance = (int)Math.floor(Math.random()*10)%2;
-		System.out.println(empAttendance);
+		
+		if (empAttendance == 1)
+		{
+			System.out.println("Daily wage for today is: "+WAGE_PER_HOUR*FULL_DAY_HRS);
+		}
 	}
 
 }
