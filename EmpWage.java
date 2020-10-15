c1b0db0785515b7717ccc61771eea19ba2f302fe
Shubham Rana <ranashubham7296@gmail.com>
2020-10-15 17:26:23 +0530

    Employee attendance check added

--- EmpWage.java
+++ EmpWage.java
@@ -3,7 +3,10 @@ package EmpWage_JAVA;
 public class EmpWage {
 	
 	public static void main(String[] args) {
+		
 		System.out.println("Welcome to Emp Wage computation");
+		int empAttendance = (int)Math.floor(Math.random()*10)%2;
+		System.out.println(empAttendance);
 	}
 
 }
